b8a573495725ac3ee2204bec741dc88e954108ad
Noah Amoo <yawnoah3n@yahoo.com>
2024-08-09 10:47:48 +0100

    Fourth Commit: Email Verification

--- auth.ts
+++ auth.ts
@@ -25,10 +25,14 @@ export const {
     },
     callbacks: {
         async signIn ({ user, account }) {
+            console.log({
+                user,
+                account,
+            })
             // Allow OAuth without email verification
             if (account?.provider !== "credentials") return true;
 
-            const existingUser = await getUserById(user.id);
+            const existingUser = await getUserById(typeof user.id);
 
             // Prevent sign in without email verification
             if (!existingUser?.emailVerified) return false;
